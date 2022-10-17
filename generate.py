8ea9b24d2bfa7946ca5f61be0ae217c37fc26548
Gareth Dwyer <gareth@ritza.co>
2022-10-17 15:11:41 +0200

    add plausible

--- generate.py
+++ generate.py
@@ -9,6 +9,7 @@ START_TEMPLATE = """<!DOCTYPE html>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.xz.style/serve/inter.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css">
+    <script defer data-domain="istheworldonfire.com" src="https://plausible.io/js/plausible.js"></script>
     <title>Is the world on fire? | {}</title>
 </head>
 <body>
